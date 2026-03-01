c6adc2503b582eff7c5c4b3f177bb9bf2824ce4c
tenpa7188 <mokos7188@gmail.com>
2026-03-02 07:39:08 +0900

    フォーマット修正

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  name_prefix = "${var.project}-${var.environment}"
-  log_bucket_name = "${local.name_prefix}-${random_id.bucket_suffix.hex}"
+  name_prefix              = "${var.project}-${var.environment}"
+  log_bucket_name          = "${local.name_prefix}-${random_id.bucket_suffix.hex}"
   log_bucket_sse_algorithm = var.environment == "prod" ? "aws:kms" : "AES256"
   log_bucket_kms_key_id    = var.environment == "prod" ? "alias/aws/s3" : null
 
